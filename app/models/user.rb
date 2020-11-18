77c2117943cac84e1f49f64372b80aced44d3174
0204ito <73625783+0204ito@users.noreply.github.com>
2020-11-18 19:56:28 +0900

    ユーザーモデル　バリデーション　修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
    validates :last_name , format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角（漢字・ひらがな・カタカナ）を入力してください' }
    validates :first_name , format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角（漢字・ひらがな・カタカナ）を入力してください' }
    validates :last_name_kana , format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを入力してください' }
-   validates :first_name_kana
+   validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを入力してください' }
    validates :birth_date
    validates :password , format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze , message: '半角英数字混合を入力してください' }
   end
