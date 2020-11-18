33d1ad81e96c1e57da3dfa18f7c2c503c088001a
0204ito <73625783+0204ito@users.noreply.github.com>
2020-11-18 20:34:27 +0900

    コンフリクト解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'ito' && password == '0204'
+    end
+  end
 end
