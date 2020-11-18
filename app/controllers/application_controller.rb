ecd8c7978b1c608af84aa4947810e974e2072275
0204ito <73625783+0204ito@users.noreply.github.com>
2020-11-18 11:40:03 +0900

    ログイン後　動作記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-  
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name,:given_name,:family_name_kana,:given_name_kana,:birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana])
   end
 end
