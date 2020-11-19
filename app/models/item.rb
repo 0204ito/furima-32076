9933c890ea5a8b031d8403c3cdd785932ac4540b
0204ito <73625783+0204ito@users.noreply.github.com>
2020-11-19 15:15:34 +0900

    itemモデル　バリデーション　修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :info
+    validates :price, numericality: { only_integer: true ,greater_than_or_equal_to: 300 ,less_than_or_equal_to: 9,999,999 }
   end
 
   with_options numericality: { other_than: 1 }  do
@@ -11,9 +12,6 @@ class Item < ApplicationRecord
   validates :shipping_free_status_id
   validates :prefecture_id
   validates :scheduled_delivery_id
-
-  
-  belong_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -25,5 +23,8 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :scheduled_delivery
+
+  belong_to :user
+  has_one_attached :image
   
 end
