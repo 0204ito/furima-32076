7a8cee753b90d64dd96182ed9acd54ab7864d568
0204ito <73625783+0204ito@users.noreply.github.com>
2020-11-20 08:42:06 +0900

    itemモデル　単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    name             {"あああ"}
+    info             {"あああ"}
+    price            {22222}
+    category_id        {2}
+    sales_status_id       {2}
+    shipping_free_status_id   {2}
+    prefecture_id             {2}
+    scheduled_delivery_id     {2}
+    association :user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
