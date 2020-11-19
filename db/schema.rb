9465f6c3922bf46a0b918e49d73ec3d68d1973e2
0204ito <73625783+0204ito@users.noreply.github.com>
2020-11-19 12:31:42 +0900

    itemsマイグレーションファイル　修正

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,10 @@ ActiveRecord::Schema.define(version: 2020_11_18_230934) do
     t.text "info", null: false
     t.integer "price", null: false
     t.bigint "user_id"
+    t.integer "category_id", null: false
+    t.integer "sales_status_id", null: false
+    t.integer "shipping_free_status_id", null: false
+    t.integer "prefecture_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
