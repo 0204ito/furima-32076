04f96103daf8dac252a01ea4aab500854041af23
0204ito <73625783+0204ito@users.noreply.github.com>
2020-11-24 18:02:10 +0900

    カード決済機能に関する　コントローラー

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,19 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :set_order
+
   def index
     @order =Order.new
-    @item = Item.find(params[:item_id])#item販売者のuser情報は取得できている
-    #orderのindex画面で、結びつくitemを表示したい。特定のレコードをDBから取得したい。Itemsコントローラーじゃないから、きちんと○○のidと指定する必要がある
     if current_user.id == @item.user_id
       redirect_to "/items"
      end
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order =Order.new (order_params)#カレントユーザー＝購入者の情報を取得して一緒に保存したい
     @order.save#(order_params)の情報が揃ったら保存してね
     if @order.valid?#バリデーションを通過してるか
+      pay_item
       redirect_to "/items"
     else
       render action: :index
@@ -24,6 +23,19 @@ class OrdersController < ApplicationController
   private
   # 全てのストロングパラメーターを1つに統合
  def order_params
-  params.require(:order).permit( :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone ).merge(user_id: current_user.id,item_id: params[:item_id])
+  params.require(:order).permit( :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone ).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
  end#ユーザー、アイテム、の記載はいらないのか？#購入者＝currentユーザーの情報も紐付けたい。
+
+ def  pay_item
+  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+  Payjp::Charge.create(
+    amount: @item.price,  # 商品の値段
+    card: order_params[:token],    # カードトークン
+    currency: 'jpy'                 # 通貨の種類（日本円）
+  )
+ end
+
+ def set_order
+  @item = Item.find(params[:item_id])#item販売者のuser情報は取得できている]
+ end#orderのindex画面で、結びつくitemを表示したい。特定のレコードをDBから取得したい。Itemsコントローラーじゃないから、きちんと○○のidと指定する必要がある
 end
